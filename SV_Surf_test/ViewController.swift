0621777a6d95595eaf229cb1d7485f13ac5d56d5
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-08-01 15:17:43 +0500

    Initial Commit

---
+++ SV_Surf_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SV_Surf_test
+//
+//  Created by Артем Павлов on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
