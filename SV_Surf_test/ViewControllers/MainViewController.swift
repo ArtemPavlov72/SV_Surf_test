c09df1b6612bcd4690dc1384257795de541b86fb
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-08-01 15:29:31 +0500

    Initial commit. Deleted Storyboard

--- SV_Surf_test/ViewControllers/MainViewController.swift
+++ SV_Surf_test/ViewControllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SV_Surf_test
 //
 //  Created by Артем Павлов on 01.08.2023.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
 
 
